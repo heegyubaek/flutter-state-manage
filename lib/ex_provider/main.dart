cb15af256eca10ff8d1be19493c38f56daa32d80
hgbaek <hgbaek@seegene.com>
2022-07-25 00:39:38 +0900

    add state management

---
+++ lib/ex_provider/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'counts.dart';
+
+void main() {
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => Counts()),
+    ],
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('main build');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ChildWidget1(notifyParent: _incrementCounter)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ChildWidget1 extends StatefulWidget {
+  const ChildWidget1({Key? key, required this.notifyParent}) : super(key: key);
+  final Function() notifyParent;
+
+  @override
+  State<ChildWidget1> createState() => _ChildWidget1State();
+}
+
+class _ChildWidget1State extends State<ChildWidget1> {
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('ChildWidget1 build');
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+            onPressed: widget.notifyParent, child: const Text('Child Widget1')),
+        ChildWidget2(notifyParent: widget.notifyParent)
+      ],
+    );
+  }
+}
+
+class ChildWidget2 extends StatefulWidget {
+  const ChildWidget2({Key? key, required this.notifyParent}) : super(key: key);
+  final Function() notifyParent;
+
+  @override
+  State<ChildWidget2> createState() => _ChildWidget2State();
+}
+
+class _ChildWidget2State extends State<ChildWidget2> {
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('ChildWidget2 build');
+    return ElevatedButton(
+        onPressed: widget.notifyParent, child: Text('Child widget2'));
+  }
+}
